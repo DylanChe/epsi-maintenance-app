36e498dc082f6cdc03953ee288641d3f570f193f
Stanoname <dylan.chesnouard@protonmail.com>
2020-02-20 11:23:46 +0100

    Update footer

--- index.php
+++ index.php
@@ -101,9 +101,9 @@
 
     <footer class="main-footer">
         <div class="float-right d-none d-sm-block">
-            <b>Version</b> 3.0.2
+            <b>Version</b> 0.0.1
         </div>
-        <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
+        <strong>Copyright &copy; 2020-2021 <a href="#">BestSoda</a>.</strong> All rights
         reserved.
     </footer>
 
